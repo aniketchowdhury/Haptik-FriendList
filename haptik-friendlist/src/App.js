97fc6e8f1a91da780dd5e17ae28a19b66945261b
Aniket Chowdhury <aniket.bhanj@pnp-hcl.com>
2021-06-21 02:52:55 +0530

    initial commit

---
+++ haptik-friendlist/src/App.js
@@ -0,0 +1,129 @@
+import './App.css';
+import {friendList} from "./mockData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faTrashAlt, faSearch} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from 'react';
+function App() {
+  const [friendInput, setFriendInput] = useState("");
+  const [friendListState, setFriendListState] = useState(friendList['friends']);
+  const [favouriteList, setFavouriteList] = useState(friendList['favorites']);
+  const displayFriendList=()=>{
+      let sortedArray = [];
+      friendListState.forEach((item,index)=>{
+        if(favouriteList.includes(item))
+        sortedArray.push(item);
+      })
+      friendListState.forEach((item)=>{
+        if(!sortedArray.includes(item))
+        sortedArray.push(item);
+      })
+          return (
+            <div style={{
+            height: "auto",
+            overflowY: "auto",
+            overflowX: "hidden",
+            maxHeight: "324px"
+            }}
+        >
+            {sortedArray.map((item,index)=>
+            {
+              let isFavourite = favouriteList.includes(item);
+              return (
+                <div key={index} style={{width:"29vw", display:"flex", padding:"10px", borderBottom:"0.5px solid lightgray"}}>
+                  <div style={{flex:"1 1 auto", textAlign:"left"}}><div>{item}</div>
+                  <span style={{fontSize: "small", color: "gray"}}>is your friend</span>
+                  </div>
+                  <button style={{marginRight:"10px", background:"none", border:"none"}} onClick={()=>editFavourite(item)}><FontAwesomeIcon icon={faStar} color={isFavourite?"black":"lightgray"}/></button>
+                  <button style={{background:"none", border:"none"}} onClick={()=>deleteFriend(item)}><FontAwesomeIcon icon={faTrashAlt}/></button>
+                </div>
+              )
+            })
+            }
+            </div>
+          )
+  }
+
+  useEffect(()=>{
+    if(!friendInput)
+    setFriendListState(friendList['friends'])
+  }, [friendInput])
+
+  const editFavourite = (value) => {
+    let result = [...favouriteList];
+    if(favouriteList.includes(value))
+    {
+      result = favouriteList.filter(x => {
+        return (x!==value)
+      })
+    friendList['favorites'] = result;
+    }
+    if(!favouriteList.includes(value)) {
+      result.push(value);
+    friendList['favorites'].push(value);
+    }
+    setFavouriteList(result);
+  }
+
+  const handleChange = (event) => {
+    if(!Number(event.target.value) && !event.target.value.match(/[&?:'"%#+,$!*@()=\\[\];/]/g))
+    {
+      setFriendInput(event.target.value);
+      searchFriend();
+    }
+  }
+
+  const enterPress = (event) => {
+    let list = [...friendListState];
+    if (event.key === 'Enter')
+    {
+      if(!friendListState.includes(friendInput))
+      {
+        friendList['friends'].push(friendInput)
+        list.push(friendInput);
+        setFriendListState(list);
+      }
+      setFriendInput("");
+    }
+  }
+
+  const deleteFriend = (value) => {
+    let choice = window.confirm("You really want to delete  "+value+"?")
+    if (choice===true)
+    {
+    let result = friendListState.filter(x =>{
+      return (x!==value)
+    })
+    friendList['friends']=result;
+    setFriendListState(result);
+  }
+  }
+
+  const searchFriend = () => {
+    let result=[];
+    result = friendListState.filter(x =>{
+      return x.toLowerCase().indexOf(friendInput.toLowerCase()) !== -1;
+    })
+    if(result && friendInput)
+    setFriendListState(result)
+  }
+
+  return (
+    <div className="App">
+      <div style={{width:"32vw", border:"1px solid lightgray", backgroundColor:"white"}}>
+        <div style={{backgroundColor:"lightgray", width:"auto", textAlign:"left", padding:"5px 10px",fontWeight:"bold"}}>Friend List</div>
+        <div style={{width: "29vw", display: "flex", borderBottom:"1px solid lightgray"}}>
+          <input placeholder="Enter your friend's name" 
+          style={{width:"inherit", border:"none",padding:"7px", flex: "1 1 auto"}}
+          value={friendInput} 
+          onChange={handleChange}
+          onKeyPress={enterPress}
+        />
+        <button style={{background:"none", border:"none"}} onClick={()=>searchFriend()}><FontAwesomeIcon icon={faSearch}/></button>
+        </div>
+        {friendListState && displayFriendList()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ haptik-friendlist/src/mockData.js
@@ -0,0 +1,4 @@
+export var friendList = {
+    "friends":["Emily","Hannah","Madison","Ashley","Sarah","Alexis","Samantha","Jessica","Elizabeth","Taylor","Lauren","Alyssa"],
+    "favorites": ["Emily","Hannah","Elizabeth","Taylor"]
+}
